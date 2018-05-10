0d816058445b424f87dacf80c1ca32ec7b7d0588
Richard Kim <rxk5123@c02w1201htd6mbp.amer.homedepot.com>
2018-05-10 17:33:10 -0400

    first commit

---
+++ src/main/java/com/funlunchpicker/FunLunchPicker/FunLunchPickerApplication.java
@@ -0,0 +1,12 @@
+package com.funlunchpicker.FunLunchPicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunLunchPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunLunchPickerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/funlunchpicker/FunLunchPicker/FunLunchPickerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.funlunchpicker.FunLunchPicker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FunLunchPickerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
