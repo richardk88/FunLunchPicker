4d0b80cd1798c22f55abdec6aff4797dee0bffff
Richard Kim <rxk5123@c02w1201htd6mbp.amer.homedepot.com>
2018-06-04 20:47:35 -0400

    pushing commit

---
+++ src/main/java/com/funlunchpicker/FunLunchPicker/dto/Restaurants.java
@@ -0,0 +1,49 @@
+package com.funlunchpicker.FunLunchPicker.dto;
+
+public class Restaurants {
+    public String name;
+    public String address;
+    public String city;
+    public String state;
+    public String zipCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
